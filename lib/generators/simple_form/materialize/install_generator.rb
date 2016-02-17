58c48f8fb422bb565e6d1e91ac13462155e2c82e
Patrick Lindsay <vindicated.fool@gmail.com>
2016-02-17 15:00:58 +0900

    Add length component generator used for Materialize Character Count
    Would like to keep this within the gem rather than generating it but
    couldn't work how to do this (load order issues)

--- lib/generators/simple_form/materialize/install_generator.rb
+++ lib/generators/simple_form/materialize/install_generator.rb
@@ -3,10 +3,11 @@ module SimpleForm
     module Generators
       class InstallGenerator < Rails::Generators::Base
         source_root File.expand_path('../templates', __FILE__)
-        desc "Creates an initializer file at config/initializers/simple_form_materialize. Which is used to configure SimpleForm to be used in conjunction with MaterializeSass"
+        desc "Creates an initializer file at config/initializers/simple_form_materialize. Which is used to configure SimpleForm to be used in conjunction with MaterializeSass. Also adds a length component which is used for Character Count feature."
 
         def create_initializer_file
           template "config/initializers/simple_form_materialize.rb"
+          template "config/initializers/simple_form_materialize/length_component.rb"
         end
       end
     end
